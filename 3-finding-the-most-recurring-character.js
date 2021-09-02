04508f0e29207f193de52cfa60475038e2861a18
jerednav <80506833+jerednav@users.noreply.github.com>
2021-09-02 08:24:26 -0700

    Create 3-finding-the-most-recurring-character.js

---
+++ 3-finding-the-most-recurring-character.js
@@ -0,0 +1,64 @@
+//Given a string of text, find and return the most recurring character. e.g
+//maxRecurringChar('aabacada') // will return 'a'
+
+//Character Mapping
+
+let charMap ={};
+
+for (let char of text) {
+  if (charMap.hasOwnProperty(char)) {
+    charMap[char]++
+  } else {
+    charMap[char] = 1;
+  }
+}
+
+//For.. in loop
+
+function maxRecurringChar(text) {
+  let charMap = {}
+  let maxCharValue = 0
+  let maxChar = ''
+  
+  for (let char of text) {
+    if (charMap.hasOwnProperty(char)) {
+      charMap[char]++
+    } else {
+       charMap[char] = 1
+    }
+  }
+  
+  for (let char in charMap) {
+    if (charMap[char] > maxCharValue) {
+      maxCharValue = charMap[char]
+      maxChar = char
+    }
+  }
+  
+  return maxChar  
+}
+
+
+
+//Forming arrays from the character map
+
+function maxRecurringChar(text) {
+  let charMap = {}
+  let charArray = []
+  let valuesArray = []
+  let maxCharValue = 0
+ 
+for (let char of text) {
+  if (charMap.hasOwnProperty(char)) {
+    charMap[char]++ 
+  } else {
+    charMap[char] = 1
+  }
+}
+  
+charArray = Objects.keys(charMap)
+  valuesArray = Object.values(charMap)
+  maxCharValue = Math.max(...valuesArray)
+  
+  return charArray[valuesArray.indexOf(maxCharValue)]
+}
