adf8e79b173249e85788d80155ee1fbf85ac03bd
jerednav <80506833+jerednav@users.noreply.github.com>
2021-09-02 08:09:57 -0700

    Update 2-counting-vowels.js

--- 2-counting-vowels.js
+++ 2-counting-vowels.js
@@ -25,4 +25,27 @@
    return counter
  }
 
+//OR REGEX
+
+function vowelsCounter(text) {
+      //Search text with Regex and store all matching instances
+      let matchingInstances = text.match(/[aeiou]/gi)
+      //g stands for global search which means that after finding
+      //the first match, it will not start over from the beginning
+      //but continue searching from the end of the previous match.
+      
+      //i stands for case insensitive search which makes the whole expression case-insensitive
+      //(for example /xyz/i would match XyZ.
+      
+      //Check if matching instances exist then calculate length
+      if (matchingInstances) {
+           return matchingInstances.length
+      } else {
+         return 0
+      }
+}
+
+
+
+
 
